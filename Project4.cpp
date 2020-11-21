0db58734791bfa1420c118661c6f2af1bb37f0ff
donlieu95 <34259296+donlieu95@users.noreply.github.com>
2020-11-21 01:29:11 -0600

    Properly commented genetic algorithm

--- Project4.cpp
+++ Project4.cpp
@@ -9,16 +9,31 @@
 #include <sys/time.h>
 #include <time.h>
 
+//CSCI 443, Evolutionary Algorithms
+//Project 4
+//Don Lieu
+//Description: Given a limit of available funds and a list of investments of varying cost, risk, and potential, this program uses a genetic algorithm to find an optimal combination of investments using available funds given the following objectives:
+////1. Maximize total potential
+////2. Minimize total risk
+////3. Minimize unused funds
+
 using namespace std;
 
-const int collection_size = 10, minRand = 1, maxRand = 2000, timeLimit = 5;
+const int collection_size = 69, minRand = 1, maxRand = 2000, timeLimit = 5;
+//Parameters can be manipulated by user
 int availableFunds = ceil(maxRand * (collection_size / 4));
+//Available funds as a function of maximum random value and collection size
 int chromPop = ceil(1.65 * pow(2, (0.21 * collection_size)));
+//Optimal population size by Goldberg
 int terminationPop = ceil( log(chromPop) );
-int mutationRate = ceil(chromPop * 0.02);
+//Final Pareto front size increases logarithmically with population size
+int mutationRate = 99;
+//Chance mutation occurs as a percentage value from 1 to 100.  Can be changed if necessary.
 int sigCoefficient = ceil( maxRand * .005 );
+//How much greater the totalPotential/totalCost - availableFunds must be to be considered significantly dominant.  Prevents premature convergence over tiny differences in optimality.
 
 struct chromosome
+//Binary string representing a set of investment choices.  1 means the investment was chosen, 0 means it was not.  All other values calculated accordingly, but totalCost cannot exceed availableFunds.
 {
 	char sequence[collection_size];
 	int totalCost;
@@ -50,43 +65,40 @@ bool cEquals(struct chromosome c1, struct chromosome c2)
 }
 
 void populate(struct investment investmentList[])
-//Polulate initial array
+//Polulate initial list of investments and prints to terminal screen
 {
-	srand (time(NULL));
-	int multiplier;
 	for(int i = 0; i < collection_size; i++)
 	{
 		int exp = rand() % 4 + 1;
-		//multiplier = rand() % 5 + 0;
 		investmentList[i].cost = rand() % maxRand + minRand;
 		investmentList[i].risk = rand() % 99 + 1;
-		//investmentList[i].potential = ceil( investmentList[i].cost * (pow( (1+(investmentList[i].risk / 100) ), exp) ) ); 
-		//investmentList[i].potential = ceil( investmentList[i].cost + (investmentList[i].cost * (investmentList[i].risk / 100) * multiplier ) );
 		investmentList[i].potential = investmentList[i].cost + ceil( investmentList[i].cost * (pow( ((100 / (100 - investmentList[i].risk) ) ), exp) ) );
 
 		cout << "Investment " << i+1 << ":" << endl;
 		cout << "Cost: $" << investmentList[i].cost << endl;
 		cout << "Risk: " << investmentList[i].risk << "%" << endl;
 		cout << "Potential: $" << investmentList[i].potential << endl;
-		//cout << "Exp: " << exp << "Multipler: " << (pow( ((100 / (100 - investmentList[i].risk) ) ), exp) ) << endl;
 		cout << endl;
 	}
 }
 
 struct chromosome initialChromosome(struct investment investmentList[])
+//Create generation 0 of parent chromosomes by random assignment of investments.
 {
 	struct chromosome newChromosome;
-	//srand (time(NULL));
 	for(int j = 0; j < collection_size; j++)
 	{
 		int randomAllele = rand() % 100 + 1;
 		if (randomAllele > 50)
 		{
 			newChromosome.sequence[j] = '1';
+
 			//Initialize cost, risk, and potential
 			newChromosome.totalCost += investmentList[j].cost;
-			if (newChromosome.totalCost > availableFunds)
 			//Cannot exceed available funds
+
+			if (newChromosome.totalCost > availableFunds)
+			//Divest if funds exceeded
 			{
 				newChromosome.totalCost -= investmentList[j].cost;
 				newChromosome.sequence[j] = '0';
@@ -102,14 +114,17 @@ struct chromosome initialChromosome(struct investment investmentList[])
 		{
 			newChromosome.sequence[j] = '0';
 		}
-		//cout << newChromosome.sequence[j];
 	}
+
 	//Calculate dominance
 	if (newChromosome.totalRisk == 0)
+	//Prevents division by zero
 	{
 		newChromosome.totalRisk = 1;
 	}
+
 	newChromosome.unusedFunds = availableFunds - newChromosome.totalCost; 
+
 	newChromosome.dominance = ceil( ( (newChromosome.totalPotential / newChromosome.totalRisk) / sigCoefficient) - (newChromosome.unusedFunds / (sigCoefficient) ) );
 	
 	if (newChromosome.dominance < 0)
@@ -118,6 +133,7 @@ struct chromosome initialChromosome(struct investment investmentList[])
 	}
 
 	newChromosome.dominant = false;
+	//Chromosomes will only be considered dominant after dominance values are compared to those of other chromosomes
 	return newChromosome;
 }
 
@@ -125,19 +141,19 @@ struct chromosome generateChild(struct chromosome parent1, struct chromosome par
 //Generate a child sequence given two parents.  Compare child sequence against original array to determine difference value
 {
 	struct chromosome child;
-	//int unusedFunds;
 	int crossoverPoint = rand() % collection_size;
+
 	//Perform single point crossover
 	for (int i = 0; i < crossoverPoint; i++)
 	{
 		child.sequence[i] = parent1.sequence[i];
 
 	}
-	//cout << endl;
 	for (int i = crossoverPoint; i < collection_size; i++)
 	{
 		child.sequence[i] = parent2.sequence[i];
 	}
+
 	//Randomly mutate one chromosome at mutation rate
 	int mutationChance = rand() % 100;
 	if (mutationChance < mutationRate)
@@ -158,11 +174,6 @@ struct chromosome generateChild(struct chromosome parent1, struct chromosome par
 	child.totalPotential = 0;
 	child.dominance = 0;
 
-	/*for (int i = 0; i < collection_size; i++)
-	{
-		cout << child.sequence[i];
-	}*/
-	//cout << endl;
 	for (int i = 0; i < collection_size; i++)
 	{
 		
@@ -170,8 +181,10 @@ struct chromosome generateChild(struct chromosome parent1, struct chromosome par
 		{
 			//Calculate cost, risk, and potential
 			child.totalCost += investmentList[i].cost;
+
+			//Cannot exceed available funds
 			if (child.totalCost > availableFunds)
-			//Cannot exceed available funds.  Divest.
+			//Divest if funds exceeded.
 			{
 				child.totalCost -= investmentList[i].cost;
 				child.sequence[i] = '0';
@@ -179,20 +192,22 @@ struct chromosome generateChild(struct chromosome parent1, struct chromosome par
 			else
 			{
 				child.totalRisk += ceil( (investmentList[i].risk) * investmentList[i].cost );
-				//cout << ceil( (investmentList[i].risk) * investmentList[i].cost ) << " added to total risk." << endl;
 				child.totalPotential += (investmentList[i].potential);
-				//cout << investmentList[i].potential << " added to total potential." << endl;
 			}
 			
 		}
 	}
-	//cout << endl;
-	//Calculate dominance value
+
+	//Calculate dominance
+
 	if (child.totalRisk == 0)
+	//Prevents division by zero
 	{
 		child.totalRisk = 1;
 	}
+
 	child.unusedFunds = availableFunds - child.totalCost;
+
 	child.dominance = ceil( ( (child.totalPotential / child.totalRisk)  / sigCoefficient) - (child.unusedFunds / (sigCoefficient) ) );
 
 	if (child.dominance < 0)
@@ -200,11 +215,8 @@ struct chromosome generateChild(struct chromosome parent1, struct chromosome par
 		child.dominance = 0;
 	}
 
-	//cout << "Total Risk: " << child.totalRisk << endl;
-	//cout << "Total Potential: " << child.totalPotential << endl;
-	//cout << "Unused Funds: " << child.unusedFunds << endl;
-	//cout << "Dominance: " << child.dominance << endl;
 	child.dominant = false;
+	//Chromosomes are only considered dominant after the dominance values are compared to those of other chromosomes
 	return child;
 }
 
@@ -216,53 +228,44 @@ int main ()
 	struct investment investmentList[collection_size];
 	bool done = false, timedOut = false;
 
+	srand (time(NULL));
+	//Pseudo-random number generator seeded with current time
+
 	clock_t timeStart = clock(), timeEnd;
+
 	populate(investmentList);
-	//Populate collection of investments
+	//Populate collection of investments and print to terminal screen.
 	
 	cout << "Investment list created.  Optimizing portfolio..." << endl;
 
 	for (int i = 0; i < chromPop; i++)
+	//Determine max dominance value
 	{
 		parents[i] = initialChromosome(investmentList);
-		//cout << endl;
 		if (parents[i].dominance > maxDominance)
 		{
 			maxDominance = parents[i].dominance;
 		}
-		/*for (int j = 0; j < i; j++)
-		{
-			if (cEquals(parents[i], parents[j]) == true)
-				//First generation must have no identical chromosomes.
-				{
-					i--;
-					break;
-				}
-		}*/
 		
 	}
 	
-	
-	//cout << "First parent generation successfully populated!  Max dominance: " << maxDominance << endl;
-	//cout << "Dominant chromosomes: ";
-	
 	for (int i = 0; i < chromPop; i++)
+	//Set parents with the max dominance value as dominant parents
 	{
 		if (parents[i].dominance >= maxDominance)
 		{
 			parents[i].dominant = true;
-			//cout << i << " ";
 		}
 	}
-	//cout << endl;
 
 	while(!done)
+	//Continue to create generations of children until population of dominant chromosomes is smaller than terminationPop or program times out.
 	{
 		paretoCount = 0;		
 		int position = 0;
-		//cout << "Generation " << generationCount << ": " << endl;
+
 		if ( (clock() - timeStart) / CLOCKS_PER_SEC >= timeLimit)
-		//Timeout
+		//Timeout.  Stop generating more children.
 		{
 			cout << "\nCOMPILE TIME LIMIT REACHED." << endl;
 			cout << "No solutions were found to be significantly more optimal than others within allotted time." << endl;
@@ -270,19 +273,22 @@ int main ()
 			timedOut = true;
 			break;
 		}
+
 		for (int i = 0; i < chromPop; i++)
+		//Select dominant parents from Pareto front only.
 		{
 			struct chromosome parent1, parent2;
 			bool p1 = false, p2 = false;
+
 			while (position < chromPop)
 			{
 				if (p1 == false)
+				//Find first parent
 				{
 					if (parents[position].dominant == true)
 					{
 						parent1 = parents[position];
 						p1 = true;
-						//cout << "Parent 1 at: " << position << endl;
 					}
 					if (position < chromPop-1)
 					{
@@ -294,12 +300,12 @@ int main ()
 					}
 				}
 				else
+				//Find second parent
 				{
 					if (parents[position].dominant == true)
 					{
 						parent2 = parents[position];
 						p2 = true;
-						//cout << "Parent 2 at: " << position << endl;
 						break;
 					}
 					if (position < chromPop-1)
@@ -312,15 +318,17 @@ int main ()
 					}
 				}
 			}
-			//cout << "Child " << i << ": ";
+
 			children[i] = generateChild(parent1, parent2, investmentList);
+			//Create child from parents
+
 			if (cEquals(children[i], children[i-1]) == true)
-			//One chromosome cannot be the same as the last.
+			//One chromosome cannot be the same as the previous.
 			{
 				i--;
 			}
-			//cout << endl;
 		}
+
 		//Replace parent generation and calculate new maxDominance
 		maxDominance = 0;
 		for (int i = 0; i < chromPop; i++)
@@ -329,9 +337,9 @@ int main ()
 			if (parents[i].dominance > maxDominance)
 			{
 				maxDominance = parents[i].dominance;
-				//cout << "New max dominance: " << maxDominance << endl;
 			}
 		}
+
 		generationCount++;
 		//Determine size of new Pareto front
 		for (int i = 0; i < chromPop; i++)
@@ -342,13 +350,15 @@ int main ()
 				paretoCount++;
 			}
 		}
+
 		if (paretoCount <= terminationPop)
+		//If Pareto front is smaller than terminationPop, the final list of optimal solutions has been found.  Stop generating children.
 		{
 			done = true;
 		}
 	}
-	
 	cout << endl;
+
 	if (timedOut == false)
 	{
 		cout << "Final list of optimal investment combinations compiled successfully within time limit.  These are:\n" << endl;
@@ -376,6 +386,7 @@ int main ()
 		}
 	}
 	else
+	//Time exceeded before a significantly optimal solution was found.  Program will try to offer the best solutions it has found within time limit.
 	{
 		cout << "Program offers the following solution(s):\n" << endl;
 		for (int i = 0; i < terminationPop; i++)
